c8fc43f1a8e909903bbd0c2b52b8d9f10103a2e4
rEjiN2 <rejin374@gmail.com>
2023-07-31 02:28:38 +0530

    validParantheseis

--- validParanthesis.js
+++ validParanthesis.js
@@ -1,1 +1,30 @@
-console.log("fi");
+function validParanthesis(str){
+    if(str.length==0 || str.length < 2){
+        return false;
+    }
+    var stack = [];
+    for(i=0;i<str.length;i++){
+       if(str[i]=="(" || str[i]=="{" || str[i]=="["){
+        stack.push(str[i]);
+        
+       }
+       else{
+        if(stack.length==0){
+            return false;
+        }
+        let last = stack.pop();
+        if(str[i]==")" && last !== "("){
+            return false;
+        }
+        if(str[i]=="}" && last !== "{"){
+            return false;
+        }
+        if(str[i]=="]" && last !=="["){
+            return false;
+        }
+       }
+    }
+    return stack.length==0;
+}
+
+console.log(validParanthesis("[{}]"));
